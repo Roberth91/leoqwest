6d543145f4f3e475ad6003143db2c401145d6fe9
Robert <rob.heald@flashtalking.com>
2015-08-12 08:41:32 +0100

    asd

--- modules/system/console/OctoberMirror.php
+++ modules/system/console/OctoberMirror.php
@@ -25,7 +25,6 @@ class OctoberMirror extends Command
     protected $directories = [
         'storage/app/uploads',
         'storage/app/media',
-        'modules/backend/formwidgets',
     ];
 
     protected $wildcards = [
